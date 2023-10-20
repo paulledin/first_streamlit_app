5c8f3bcb05fcf8c7f3ad6e811bd3dfc21ccbc6b3
Paul Ledin <paul_ledin@uwalumni.com>
2023-10-20 16:01:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,7 @@ streamlit.dataframe(fruits_to_show)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+streamlit.header('Fruity Vice Fruit Advice!')
+streamlit.text(fruityvice_response.json())
+
 
