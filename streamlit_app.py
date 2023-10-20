151f07543703120987e94f24050c2851277aa177
Paul Ledin <paul_ledin@uwalumni.com>
2023-10-20 16:04:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,4 +28,7 @@ streamlit.text(fruityvice_response)
 streamlit.header('Fruity Vice Fruit Advice!')
 streamlit.text(fruityvice_response.json())
 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
 
