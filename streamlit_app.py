42c827d7f3b120eeb4f05cb1cc64b400fa48b871
Paul Ledin <paul_ledin@uwalumni.com>
2023-10-23 15:35:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,6 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruite load list contains:")
+streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
 
