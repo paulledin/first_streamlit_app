ede4a235e8ba06cc179f08e51af1d41daa5eb25b
Paul Ledin <paul_ledin@uwalumni.com>
2023-10-23 15:52:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding', add_my_fruit)
 
-my_curr.execute("INSERT INTO fruit_load_list VALUES ('from streamlit')")
+my_cur.execute("INSERT INTO fruit_load_list VALUES ('from streamlit')")
 
 
 
